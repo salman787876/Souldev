d691be1e0f289a929f11fb42bf92fc846719bf93
salman787876 <salman00778876@gmail.com>
2026-03-30 14:35:03 +0300

    Update password.js

--- 3️⃣ /tools/password.js
+++ 3️⃣ /tools/password.js
@@ -1,1 +1,9 @@
-
+const hashPassBtn = document.getElementById('hashPassBtn');
+hashPassBtn.addEventListener('click', async ()=>{
+  const input = document.getElementById('passInput').value;
+  const enc = new TextEncoder();
+  const hashBuffer = await crypto.subtle.digest('SHA-256', enc.encode(input));
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2,'0')).join('');
+  document.getElementById('hashedPass').textContent = hashHex;
+});
