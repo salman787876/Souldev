8fc83d45ff07b9a070fdbe53d77c846434ab47d3
salman787876 <salman00778876@gmail.com>
2026-04-15 20:10:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function generateOrderID() {
   return "PRIME-" + Date.now().toString().slice(-6);
 }
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "./firebase";
+
+async function createOrder(name, service) {
+  const orderId = generateOrderID();
+
+  await addDoc(collection(db, "orders"), {
+    name: name,
+    service: service,
+    orderId: orderId,
+    status: "pending"
+  });
+
+  alert("رقم طلبك: " + orderId);
+}
